006b6071020c0829f7872c46d7843c399127afe8
Jeg Xiao <3556272257@qq.com>
2026-04-13 22:13:10 +0800

    建设中

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projects = [
     { name: "本页面", url: "https://jegxiao.github.io/index.html" },
     { name: "何意味", url: "https://jegxiao.github.io/何意味/何意味.html" },
     { name: "我要验牌", url: "https://jegxiao.github.io/我要验牌/index.html" },
-    { name: "……建设中", url: "#" },
+    { name: "……建设中", url: "我要验牌/index.html" },
     { name: "……建设中", url: "#" },
     { name: "……建设中", url: "#" },
     { name: "……建设中", url: "#" },
